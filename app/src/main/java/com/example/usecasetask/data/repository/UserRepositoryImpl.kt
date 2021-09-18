0417956344e11e060efbafa5fb0bdd57628bfaf1
OlegDeveloper <tmereshkov@gmail.com>
2021-09-18 22:47:20 +0300

    User.kt

--- app/src/main/java/com/example/usecasetask/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/example/usecasetask/data/repository/UserRepositoryImpl.kt
@@ -2,35 +2,24 @@ package com.example.usecasetask.data.repository
 
 import android.content.Context
 import android.content.SharedPreferences
+import com.example.usecasetask.data.storage.UserStorage
 import com.example.usecasetask.domain.model.SaveUserNameParam
 import com.example.usecasetask.domain.model.UserName
 import com.example.usecasetask.domain.model.repository.UserRepository
 import java.security.AccessControlContext
 
-private const val SHARED_PREFS_NAME = "shared_prefs_name"
-private const val KEY_FIRST_NAME = "first_name"
-private const val KEY_LAST_NAME = "last_name"
-private const val DEFAULT_NAME = "Default last name"
+
+class UserRepositoryImpl(private val userStorage: UserStorage) : UserRepository {
 
 
-
-class UserRepositoryImpl(context: Context):UserRepository {
-
-    private val sharedPreferences = context.getSharedPreferences(SHARED_PREFS_NAME, Context.MODE_PRIVATE)
-
-    override fun saveName(saveParam: SaveUserNameParam): Boolean{
-
-        sharedPreferences.edit().putString(KEY_FIRST_NAME, saveParam.name).apply()
-
-        return true
+    override fun saveName(saveParam: SaveUserNameParam): Boolean {
+        val rezult = userStorage.save(saveParam)
+        return rezult
     }
 
     override fun getName(): UserName {
-
-        val firstName = sharedPreferences.getString(KEY_FIRST_NAME, "")?: ""
-        val lastName = sharedPreferences.getString(KEY_LAST_NAME, DEFAULT_NAME)?: DEFAULT_NAME
-
-        return UserName(firstName = firstName, lastName = lastName)
+        val user = userStorage.get()
+        return user
 
     }
 }
---
+++ app/src/main/java/com/example/usecasetask/data/storage/User.kt
@@ -0,0 +1,2 @@
+package com.example.usecasetask.data.storage
+
